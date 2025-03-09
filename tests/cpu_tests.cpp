d7fe56a52e48dba46f3053f2bd1154b4d895a683
beserchris <beserchris@gmail.com>
2025-03-09 15:20:55 -0700

    aaaaa

--- tests/cpu_tests.cpp
+++ tests/cpu_tests.cpp
@@ -1,10 +1,13 @@
 // tests/cpu_tests.cpp
 #include <catch2/catch_test_macros.hpp>
-#include "cpu.hpp"  // Your CPU header
+#include "../src/cpu.hpp"  // Your CPU header
+
+cpu_s cpu;
+byte_t memory[1024*1024*64];
 
 TEST_CASE("Sanity Check: CPU Initial State", "[cpu]") {
-    cpu_s cpu;  // Create CPU instance
-    cpu.A = 42;
+    cpu.A = 1;
+    cpu_init(&cpu, memory);
     // Minimal check for accumulator initialization
     REQUIRE(cpu.A == 0x00);
 }
